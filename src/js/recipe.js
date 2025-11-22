48634d571f383653e960036dcd174d33a1d8dc8a
gw-devf <gmbwolf.contato@gmail.com>
2025-11-22 14:18:13 -0300

    fix: add style button and bad request recipe page

--- src/js/recipe.js
+++ src/js/recipe.js
@@ -1,6 +1,18 @@
 import getData from "./utils/getData.js";
 import findCategoryName from "./utils/findCategoryName.js";
-import recipe from "./components/recipe.js";
+import recipeComponent from "./components/recipe.js";
+import badRequest from "./components/badRequest.js";
+
+const mountRecipe = (recipeClicked, categories, recipeEl) => {
+  recipeEl.innerHTML = "";
+
+  if (recipeClicked !== null && categories !== null) {
+    const category = findCategoryName(categories, recipeClicked.category);
+    recipeEl.innerHTML += recipeComponent(recipeClicked, category);
+  } else {
+    recipeEl.innerHTML = badRequest();
+  }
+};
 
 window.onload = async () => {
   if (localStorage.getItem("recipeId") === null) {
@@ -12,10 +24,9 @@ window.onload = async () => {
   const [recipeClicked] = recipes.filter(
     (recipe) => recipe.id === Number(localStorage.getItem("recipeId")),
   );
-  const category = findCategoryName(categories, recipeClicked.category);
   const recipeEl = document.querySelector(".recipe");
 
-  recipeEl.innerHTML = recipe(recipeClicked, category);
+  mountRecipe(recipeClicked, categories, recipeEl);
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
